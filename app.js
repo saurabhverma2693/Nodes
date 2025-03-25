476e4bb9d94f39f39e6a2b37440fe56968234af7
saurabh <saurabhverma2693@gmail.com>
2025-03-25 18:34:43 +0530

    Responses

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http = require('http');
 let server = http.createServer((req,res)=>{
 console.log(req.url,req.method,req.headers);
 // process.exit();
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>My First page</title></head>');
+res.write('<body> <h1>Hello from node js.......</h1> </body>');
+res.write('</html>');
+res.end();
 });
 
 server.listen(5000);
