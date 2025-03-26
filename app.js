ce083f48c27390f051ec5865bd6d1dbf7f022d42
saurabh <saurabhverma2693@gmail.com>
2025-03-26 14:55:03 +0530

    Blocking & Non Blocking Code

--- app.js
+++ app.js
@@ -22,17 +22,19 @@ if(url === '/message' && method === 'POST'){
         body.push(chunk);
     });
 
-    req.on('end',()=>{
+   return req.on('end',()=>{
 const parsedBody = Buffer.concat(body).toString();
 const message = parsedBody.split('=')[1];
-fs.writeFileSync('message.txt',message); 
+fs.writeFile('message.txt',message,err => {
+    res.statusCode = 302;
+    res.setHeader('Location','/');
+    return res.end();
+}); 
     });
-
-// fs.writeFileSync('message.txt','Dummy');
-res.statusCode = 302;
-res.setHeader('Location','/');
-return res.end();
 }
+// fs.writeFileSync('message.txt','Dummy');
+
+
 
 res.setHeader('Content-Type','text/html');
 res.write('<html>');
