257a6769fb186e6106ab5b18019e70117daedc42
saurabh <saurabhverma2693@gmail.com>
2025-03-26 14:12:15 +0530

    Parse Request Body

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ if(url === '/'){
 }
 
 if(url === '/message' && method === 'POST'){
-fs.writeFileSync('message.txt','Dummy');
+
+    const body = [];
+    req.on('data',(chunk)=>{
+        console.log(chunk);
+        body.push(chunk);
+    });
+
+    req.on('end',()=>{
+const parsedBody = Buffer.concat(body).toString();
+const message = parsedBody.split('=')[1];
+fs.writeFileSync('message.txt',message); 
+    });
+
+// fs.writeFileSync('message.txt','Dummy');
 res.statusCode = 302;
 res.setHeader('Location','/');
 return res.end();
