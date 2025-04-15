1236bd74a4c0209cf7ba8b7dbcc9b10a1aa59d6d
saurabh <saurabhverma2693@gmail.com>
2025-04-15 14:43:50 +0530

    how Middleware Works

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 const app = express();
 
-// const routes = require('./routes');
+app.use((req,res, next)=>{
+    console.log('In the middleware.......');
+    next();
+    
+    // next();
+    // next();
+});
 
 
+app.use((req,res, next)=>{
+    console.log('In another middleware.......');
+    res.send('<h1>Hello from express.....</h1>');
+});
 
-const server = http.createServer(app);
+// app.use((req,res, next)=>{
+//     console.log('In third other middleware.......'); // My new Middlewre  and their value.
+//     next();
+// });
+
+// app.use((req,res, next)=>{
+//     console.log('In the fourth middleware.......');
+// });
+
+
 
 // let server = http.createServer(routes);
-
-server.listen(5001);
+// const server = http.createServer(app);
+// server.listen(5001);
+app.listen(5001);
