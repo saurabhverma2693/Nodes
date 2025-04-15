f696364459f22d954a217acf78d441a7ba44a2ff
saurabh <saurabhverma2693@gmail.com>
2025-04-15 15:18:05 +0530

    Parse Incoming Request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // app.use((req,res, next)=>{
@@ -11,9 +12,10 @@ const app = express();
 //     // next();
 // });
 
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/',(req,res, next)=>{
-    console.log('This always runs.....');
+    // console.log('This always runs.....');
     next();
     
 });
@@ -21,13 +23,18 @@ app.use('/',(req,res, next)=>{
 
 
 app.use('/add-product',(req,res, next)=>{
-    console.log('In  middleware.......');
-    res.send('<h1>Add Product Page........</h1>');
+    // console.log('In  middleware.......');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button> </form>');
+});
+
+app.use('/product',(req,res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
 });
 
 
 app.use('/',(req,res, next)=>{
-    console.log('In another middleware.......');
+    // console.log('In another middleware.......');
     res.send('<h1>Hello from express.....</h1>');
 });
 
