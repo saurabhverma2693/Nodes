40e0d3743d127ff5541217fb21afb792c1b03d74
saurabh <saurabhverma2693@gmail.com>
2025-04-16 14:00:59 +0530

    use app.post instead of app.use

--- app.js
+++ app.js
@@ -27,7 +27,12 @@ app.use('/add-product',(req,res, next)=>{
     res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button> </form>');
 });
 
-app.use('/product',(req,res, next)=>{
+// app.use('/product',(req,res, next)=>{
+//     console.log(req.body);
+//     res.redirect('/');
+// });
+
+app.post('/product',(req,res, next)=>{
     console.log(req.body);
     res.redirect('/');
 });
