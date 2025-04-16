8e695f21caeca73763ccf875924b3a9359018d3c
saurabh <saurabhverma2693@gmail.com>
2025-04-16 17:31:26 +0530

    Return 404 Page

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // const http = require('http');
-
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -11,6 +11,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use((req,res,next) => {
-    res.status(404).send('<h1>Page not found.......</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 app.listen(5001);
