31797291a1796143e214865d099e02d4e25e0d84
saurabh <saurabhverma2693@gmail.com>
2025-04-16 14:21:40 +0530

    Use Express Router

--- app.js
+++ app.js
@@ -3,58 +3,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
-// app.use((req,res, next)=>{
-//     console.log('In the middleware.......');
-//     next();
-                                                       // Dummy Middleware
-//     // next();
-//     // next();
-// });
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/',(req,res, next)=>{
-    // console.log('This always runs.....');
-    next();
-    
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-
-
-app.use('/add-product',(req,res, next)=>{
-    // console.log('In  middleware.......');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button> </form>');
-});
-
-// app.use('/product',(req,res, next)=>{
-//     console.log(req.body);
-//     res.redirect('/');
-// });
-
-app.post('/product',(req,res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/',(req,res, next)=>{
-    // console.log('In another middleware.......');
-    res.send('<h1>Hello from express.....</h1>');
-});
-
-// app.use((req,res, next)=>{
-//     console.log('In third other middleware.......'); // My new Middlewre  and their value.
-//     next();
-// });
-
-// app.use((req,res, next)=>{
-//     console.log('In the fourth middleware.......');
-// });
-
-
-
-// let server = http.createServer(routes);
-// const server = http.createServer(app);
-// server.listen(5001);
 app.listen(5001);
