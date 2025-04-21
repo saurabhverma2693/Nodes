903e75a18ebec96e47987fef614bea8ecfea50da
saurabh <saurabhverma2693@gmail.com>
2025-04-21 17:16:57 +0530

    Output Dynamic Content

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,50 @@
-const express = require('express');
+
 const path = require('path');
+
+const express = require('express');
+
 const rootDir = require('../util/path');
-const { title } = require('process');
+
 const router = express.Router();
+
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product',(req,res, next)=>{
-    // console.log('In  middleware.......');
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
-router.post('/add-product',(req,res, next)=>{
-    // console.log(req.body);
-    products.push({title:req.body.title});
-    res.redirect('/');
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-
-// module.exports = router;
 exports.routes = router;
 exports.products = products;
+
+
+// const express = require('express');
+// const path = require('path');
+// const rootDir = require('../util/path');
+// const { title } = require('process');
+// const router = express.Router();
+// const products = [];
+
+// // /admin/add-product => GET
+// router.get('/add-product',(req,res, next)=>{
+//     // console.log('In  middleware.......');
+//     res.sendFile(path.join(rootDir,'views','add-product.html'));
+// });
+
+// // /admin/add-product => POST
+// router.post('/add-product',(req,res, next)=>{
+//     // console.log(req.body);
+//     products.push({title:req.body.title});
+//     res.redirect('/');
+// });
+
+
+// // module.exports = router;
+// exports.routes = router;
+// exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const rootDir = require('../util/path');
-const router = express.Router();
 const adminData = require('./admin');
+const router = express.Router();
+
 
 router.get('/',(req,res, next)=>{
-    res.render('shop');
+    const products = adminData.products;
+    res.render('shop', {prods:products,docTitle:'Shop'});
     // console.log('shop.js',adminData.products)
     // // res.sendFile('/views/shop.html'); // This code Don't Work
     // res.sendFile(path.join(rootDir,'views','shop.html'));
