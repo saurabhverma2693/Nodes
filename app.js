4719c345f03a060b400cf75a3d02846f1d415f54
saurabh <saurabhverma2693@gmail.com>
2025-04-24 16:34:12 +0530

    Work with handlebars

--- app.js
+++ app.js
@@ -1,37 +1,14 @@
-
-// const path = require('path');
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-
-// app.set('view engine', 'pug');
-// app.set('views', 'views');
-
-// const adminData = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/admin', adminData.routes);
-// app.use(shopRoutes);
-
-// app.use((req, res, next) => {
-//     res.status(404).render('404', {pageTitle: 'Page Not Found'});
-// });
-
-// app.listen(5001);
-
-//...........................................................................................................................................
 const http = require('http');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const  expressHbs = require('express-handlebars');
 const app = express();
 
-app.set('view engine','pug');
+
+app.engine('hbs', expressHbs());
+app.set('view engine','hbs');
+// app.set('view engine','pug');
 app.set('views','views');
 
 const adminData = require('./routes/admin');
@@ -50,4 +27,3 @@ app.use((req,res,next) => {
 });
 app.listen(5001);
 
-// VIDEO NO 85 NOT COMPLETED
