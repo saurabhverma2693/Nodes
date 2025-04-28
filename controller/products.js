de3e677d6716fef51213ecf78f58ee1133bc5779
saurabh <saurabhverma2693@gmail.com>
2025-04-28 18:17:18 +0530

    Add Product Model

--- controller/products.js
+++ controller/products.js
@@ -1,6 +1,7 @@
+const Product = require('../models/product');
 const express = require('express');
 const router = express.Router();
-const products = [];
+
 
 exports.getAddProduct =  (req, res, next) => {
   res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
@@ -8,12 +9,13 @@ exports.getAddProduct =  (req, res, next) => {
 
 // /admin/add-product => POST
 exports.postAddProduct = router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 })
 
 exports.getProducts = (req, res, next) => {
-
+const products = Product.fetchall();
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
