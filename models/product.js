ac187f7075e41dc501bfc1fc80b4078a31d2c5bf
saurabh <saurabhverma2693@gmail.com>
2025-05-01 00:45:01 +0530

    Storing Product Data

--- models/product.js
+++ models/product.js
@@ -23,8 +23,12 @@ const getProductFromfILE = cb => {
 }
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title,imageUrl,price,description) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+    
   }
 
   save() {
