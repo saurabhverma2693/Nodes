43f1be6a19008ba6b20cdcf0f70d660a59f44dfd
saurabh <saurabhverma2693@gmail.com>
2025-05-07 19:02:15 +0530

    Section 12.186 - Database Connectivity & Add Data in Products

--- util/database.js
+++ util/database.js
@@ -1,16 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+let _db;
 
 const mongoConnect = (callback)=>{
-    MongoClient.connect('mongodb+srv://saurabhverma2693:Sv%402025@cluster0.rutufru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    MongoClient.connect('mongodb+srv://saurabhverma2693:Sv%402025@cluster0.rutufru.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
     // MongoClient.connect('mongodb+srv://saurabhverma2693:Sv@2025@cluster0.rutufru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(client => {
         console.log('Connected!');
+        _db = client.db();
         callback(client);
     })
     .catch(err =>{
         console.log(err);
+        throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = ()=>{
+    if(_db){
+        return _db;
+    }
+    throw 'No Database Found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
