1343e1098527d714775c6e7f9c7632377b04f756
saurabh <saurabhverma2693@gmail.com>
2025-05-12 22:23:45 +0530

    fetch single product

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(title, imageUrl,price, description);
   product
   .save()
   .then(result =>{
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,30 +1,41 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
-  .then(products =>{
-    res.render('shop/proct-list',{
-      prods:products,
-      pageTitle:'All Products',
-      path:'/products'
+    .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err=>{
-    console.log(err);
-  });
-}
-   
+};
+
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products => {
+  //     res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
+  Product.findById(prodId)
+    .then(product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
+
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll()
