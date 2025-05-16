79ae614cff0aad6333b84e5b9cf778c9b1c0e3cb
saurabh <saurabhverma2693@gmail.com>
2025-05-16 15:51:03 +0530

    Initialize session middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -19,6 +20,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:'my secret', resave:false,saveUninitialized:false}));
 
 app.use((req, res, next) => {
   User.findById('6825d1d33a2debb9e06c1cf6')
