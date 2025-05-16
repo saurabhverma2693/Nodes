94d1fc7414aa126bdd615af523e3c11cfad9d917
saurabh <saurabhverma2693@gmail.com>
2025-05-16 15:39:02 +0530

    Configure Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,15 @@
 exports.getLogin = (req, res, next) => {
-   const isLoggedIn =  req.get('Cookie').split(';')[0].trim().split('=')[1];
+//    const isLoggedIn =  req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie','loggedIn=true');
+    // res.setHeader('Set-Cookie','loggedIn=true; Secure');
+    res.setHeader('Set-Cookie','loggedIn=true; HttpOnly');
     res.redirect('/');
   };
 
